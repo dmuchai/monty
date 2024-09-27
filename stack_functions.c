8e8dfceaeb22a3fa20fdd7865a3809a13d901484
Your Name <dmmuchai@gmail.com>
2024-09-27 07:17:51 -0700

    monty

--- stack_functions.c
+++ stack_functions.c
@@ -71,7 +71,11 @@ void pop_from_stack(stack_t **stack, unsigned int line_number)
 void peek_stack(stack_t **stack, unsigned int line_number)
 {
 	if (stack == NULL || *stack == NULL)
-		handle_runtime_error(6, line_number);
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+		
 	printf("%d\n", (*stack)->n);
 }
 
