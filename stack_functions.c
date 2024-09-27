eeb6495d25494bed1481887237417ad37eaa52cd
Your Name <dmmuchai@gmail.com>
2024-09-27 07:46:00 -0700

    monty

--- stack_functions.c
+++ stack_functions.c
@@ -72,7 +72,7 @@ void peek_stack(stack_t **stack, unsigned int line_number)
 {
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		printf("L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 		
